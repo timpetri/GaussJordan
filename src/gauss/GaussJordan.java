071b8843b07fa6990a2847db92c4bd3fbf0182d9
Tim <tapetri@wpi.edu>
2015-04-30 13:57:29 -0400

    Semi-final commit

--- src/gauss/GaussJordan.java
+++ src/gauss/GaussJordan.java
@@ -9,6 +9,16 @@ public class GaussJordan {
 	public GaussJordan(String s) {
 		this.inputString = s.trim();
 	}
+	
+	public GaussJordan(double[][] inputMatrix) {
+		
+		int row = inputMatrix.length;
+		int col = inputMatrix[0].length;
+		
+		this.matrix = new double[row][col];
+		
+		this.matrix = inputMatrix;
+	}
 
 	/**
 	 * Parses the input string to populate other attributes of class.
@@ -28,11 +38,11 @@ public class GaussJordan {
 			}
 		}
 	}
-	
-	
+
+
 	public String produceSolutionString() {
 		String solution = "";
-		
+
 		if (!this.validSolution()) {
 			solution = "There is no solution.\n";
 		}
@@ -44,7 +54,7 @@ public class GaussJordan {
 			solution = "There is a unique solution:\n";
 			solution += produceUniqueSolution();
 		}
-		
+
 		return solution;		
 	}
 
@@ -55,47 +65,62 @@ public class GaussJordan {
 	private String produceUniqueSolution() {
 		String solution = "";
 		int numCol = matrix[0].length;
-		
+
 		for (int row = 0, col = 0; col < (this.matrix[0].length - 1) && row < (this.matrix.length); row++, col++) {
 			solution += "x" + (col+1) + " = " + matrix[row][numCol-1] + "\n";
 		}
-		
+
 		return solution;
 	}
 
 
 	/**
 	 * The fuckery that is producing a non-unique solution string.
+	 * Assumes that the solution is not invalid.
 	 * @return
 	 */
 	private String produceInifinteSolution() {
-		
+
 		String solution = "";
+		int numRow = matrix.length;
 		int numCol = matrix[0].length;
-		
+
 		int row = 0, col = 0;
-		
+
 		// traverse down diagonal add solution for each variable (either free or as equation)
-		while ( col < (this.matrix[0].length - 1) && row < (this.matrix.length)) {
-			System.out.println(solution);
-			if (matrix[row][col] == 0.0 || matrix[row][col] == -0.0) {
+		while ( col < (numCol - 1) && row < numRow) {
+
+			// System.out.println(solution);
+
+			// if not a pivot, variable #(col+1) is free
+			if (Math.abs(matrix[row][col]) == 0.0) {
 				solution += "x" + (col+1) + " is free.\n";
+
+				// this column can no longer contain an element, so move to the left in row
 				col++;
 			}
+			// if a pivot, produce what it is in terms of constant and known variable
 			else {
+
+				// variable = constant
 				solution += "x" + (col+1) + " = " + matrix[row][numCol-1] + " ";
+
+				// and in terms of other variables
 				for (int col2 = numCol-2; col2 > col; col2--) {
+
 					if (Math.abs(matrix[row][col2]) != 0.0) {
 						solution += doubleToOppositeString(matrix[row][col2]) + "x"+ (col2+1);
 					}
 				}
-				solution += "\n";
+				// move down to next potential pivot element
 				row++;
 				col++;
+				solution += "\n";
 			}
 		}
-		
-		// add final free variables
+
+		// add final free variables when bottom of matrix has been reached but there are
+		// still columns that were not covered. They have to be free variables.
 		while (col < numCol-1) {
 			solution += "x" + (col+1) + " is free.\n";
 			col++;
@@ -105,6 +130,11 @@ public class GaussJordan {
 
 
 	/**
+	 * Produces the string that you get when moving a coefficient to other side of equal sign.
+	 * Ignores zero coefficients and adds *-sign to non-zeros.
+	 * 
+	 * 1 + d = 0 -> 1 = -d
+	 * 
 	 * @param d
 	 * @return
 	 */
@@ -119,21 +149,26 @@ public class GaussJordan {
 
 
 	/**
+	 * Returns true if this matrix has free variables.
+	 * 
 	 * @return
 	 */
 	private boolean hasFreeVariables() {
+
 		
-		// easy first check
 		int numRow = this.matrix.length;
 		int numCol = this.matrix[0].length;
+
+		// easy first check
 		if ((numCol - numRow) > 1)
 			return true;
 		
-			for (int row = 0, col = 0; col < (this.matrix[0].length - 1); row++, col++) {
-				if (this.matrix[row][col] == 0.0)
-					return true;
-			}
-		
+		// if a potential pivot element is zero, the corresponding variable is free
+		for (int row = 0, col = 0; col < (numCol - 1); row++, col++) {
+			if (this.matrix[row][col] == 0.0)
+				return true;
+		}
+
 		return false;
 	}
 
@@ -145,58 +180,58 @@ public class GaussJordan {
 		int lead = 0;
 		int rowCount = this.matrix.length;
 		int colCount = this.matrix[0].length;
-		
+
 		for (int r = 0; r < rowCount; r++) {
-			
+
 			// if past last column, stop
 			if (lead >= colCount)
 				break;
-			
+
 			// set index that will go through column looking non-zero, starting with row below previous iteration
 			int i = r;
-			
+
 			// if element in row i and and column lead is not zero, look further down column
 			while (matrix[i][lead] == 0.0 ) {
 				i = i + 1;
-				
+
 				// if at last row, move one column to right, and start looking down from row r again
 				if (i == rowCount) {
 					i = r;
 					lead = lead + 1;
-					
+
 					// if bottom right element of array has been reach, stop
 					if (lead == colCount)
 						return; // TODO: break or return?
 				}
 			}
-			
+
 			// swap row r (where started) and i (where first non-zero element is found) if necessary
 			if (r != i) {
 				double[] temp = this.matrix[r];
 				this.matrix[r] = this.matrix[i];
 				this.matrix[i] = temp;
 			}
-			
-			
+
+
 			matrix[r] = scaleRowWithPivot(lead, matrix[r]);
-			
+
 			// eliminate other entries in same column by subtracting matrix[r]*number from its respective row
 			for (int j = 0; j < rowCount; j++) {
-				
+
 				// do not eliminate row r itself
 				if (j != r && matrix[j][lead] != 0.0) {
 					matrix[j] = eliminateEntry(matrix[j][lead], matrix[r], matrix[j]);
 				}
-				
+
 				System.out.println(this);
 			}
-			
+
 			// increase column
 			lead = lead + 1;
 		}
-		
+
 	}
-	
+
 	/**
 	 * Scales the row by dividing each element by the value of element at pivotIndex.
 	 * 
@@ -205,54 +240,60 @@ public class GaussJordan {
 	 * @return
 	 */
 	private double[] scaleRowWithPivot(int pivotIndex, double[] row) {
-		
+
 		double pivotValue = row[pivotIndex];
-		
+
 		for (int i = pivotIndex; i < row.length; i++) {
 			row[i] = row[i]/pivotValue;
 			System.out.println("[scaleRowWithPivot] Scaled value = " + row[i]);
 		}
-		
+
 		return row;
 	}
 
 
 	/**
-	 * @param 
-	 * @param es
-	 * @param es2
-	 * @return
+	 * Takes two columns and a coefficient and subtracts one column multipled by coefficient from the other.
+	 * 
+	 * @param coefficient
+	 * @param pivotColumn
+	 * @param elimColumn
+	 * @return new column
 	 */
 	private double[] eliminateEntry(double coefficient, double[] pivotColumn, double[] elimColumn) {
 		System.out.println("[eliminateEntry]: coefficient = " + coefficient);
 		System.out.println("[eliminateEntry]: pivotColumn = " + pivotColumn);
 		System.out.println("[eliminateEntry]: elimColumn = " + elimColumn);
-		
+
 		double[] result = new double[pivotColumn.length];
-		
+
 		for (int i = 0; i < result.length; i++) {
 			result[i] = elimColumn[i] - coefficient*pivotColumn[i];
 			System.out.println("[eliminateEntry]: Entry = " + result[i]);
 		}
-		
+
 		return result;
 	}
 
-
+	
+	/**
+	 * Produces true if the given matrix (assumed to be in RRE form) has a valid solution.
+	 * @return
+	 */
 	public boolean validSolution() {
 		int i=0;
-		
+
 		while(i<this.matrix.length) {
 			if (!validRow(matrix[i]))
 				return false;
 			i++;
 		}
 		return true;
-		
 	}
 
 
 	/**
+	 * Returns true if the given row has a valid solution.
 	 * 
 	 * @return
 	 */
@@ -261,14 +302,14 @@ public class GaussJordan {
 			System.out.println("Row ok!");
 			return true;
 		}
-		
+
 		for (int i=0; i<row.length-1; i++) {
 			if (row[i] != 0.0 || row[i] != -0.0) {
 				System.out.println("Row ok!");
 				return true;
 			}
 		}
-		
+
 		return false;
 	}
 
@@ -282,10 +323,10 @@ public class GaussJordan {
 			}
 			s += "\n";
 		}
-			
+
 		return s;
 	}
-	
+
 	/**
 	 * Returns matrix.
 	 * @return
@@ -293,21 +334,19 @@ public class GaussJordan {
 	public double[][] getMatrix() {
 		return this.matrix;
 	}
-
-
+	
 	/**
-	 * Returns occurrences of given character in given string.
-	 * @param s string
-	 * @param c character
-	 * @return occurrences
+	 * Sets the matrix using given matrix.
+	 * @param givenMatrix
 	 */
-	public static int countOccurences(String s, char c) {
-		int counter = 0;
-		for( int i=0; i<s.length(); i++ ) {
-			if(s.charAt(i) == c)
-				counter++;
-		}	
-		return counter;
+	public void setMatrix(double[][] givenMatrix) {
+		
+		int row = givenMatrix.length;
+		int col = givenMatrix[0].length;
+		
+		this.matrix = new double[row][col];
+		
+		this.matrix = givenMatrix;
 	}
-
 }
+
--- src/gauss/Runner.java
+++ src/gauss/Runner.java
@@ -4,36 +4,75 @@ import javax.swing.JOptionPane;
 
 public class Runner {
 
+	
+	private static double[][] problem1 = {
+		// x = 1, y = 2, z = 3
+		{ 1,  2, 3, 14 },  // 1x + 2y + 3z = 14
+		{ 1, -1, 1,  2 },  // 1x - 1y + 1z = 2
+		{ 4, -2, 1,  3 }   // 4x - 2y + 1z = 3
+	};
+
+	private static double[][] problem2 = {
+		// x = 1, y = 2, z = 3
+		{ 2,  3, -4, -4 },  // 2x + 3y - 4z = -4
+		{ 1, -2,  1,  0 },  // 1x - 2y + 1z =  0
+		{-1,  1,  2,  7 }   // -1x + 1y + 2z = 7
+	};
 
 
 	public static void main(String[] args) {
+		
+		GaussJordan gj = new GaussJordan(problem1);
+		System.out.println(gj);
+		gj.toReducedRowEchelonForm();
+		System.out.println(gj);
+		System.out.println(gj.produceSolutionString());
+		
+		gj = new GaussJordan(problem2);
+		System.out.println(gj);
+		gj.toReducedRowEchelonForm();
+		System.out.println(gj);
+		System.out.println(gj.produceSolutionString());
+		
 
-		int i = JOptionPane.showConfirmDialog(null, "Would you like to enter a matrix?", "Quit? Y/N", JOptionPane.YES_NO_OPTION);
+		System.exit(0);
+	}
 
-		while (i == 0) {
-			
-			System.out.println(i);
+
+
+	public void properUI() {
+		
+		// for entering multiple matrices
+		int enterManually = 0;
+
+		do {	
+
 			String instructions = "Enter the matrix as: 1,2,3;4,5,6;7,8,9";
 			String input = JOptionPane.showInputDialog(instructions);
 
-
 			GaussJordan gj = new GaussJordan(input);
 
+			// deal with input string
 			gj.parseInput();
-			System.out.println(gj);
 
-			gj.toReducedRowEchelonForm();
-			System.out.println(gj);
-			System.out.println(gj.produceSolutionString());
-			
+			// display matrix
 			JOptionPane.showMessageDialog(null, gj.toString());
+
+			// run Gauss Jordan elimination algorithm
+			gj.toReducedRowEchelonForm();
+
+			// display RRE matrix
+			JOptionPane.showMessageDialog(null, gj.toString());
+
+			// display solution
 			JOptionPane.showMessageDialog(null, gj.produceSolutionString());
-			
-			i = JOptionPane.showConfirmDialog(null, "Would you like to enter another matrix?", "Quit? Y/N", JOptionPane.YES_NO_OPTION);
-		}
+
+			// new matrix?
+			enterManually = JOptionPane.showConfirmDialog(null, "Would you like to enter another matrix?", "Quit? Y/N", JOptionPane.YES_NO_OPTION);
+
+		} while (enterManually == 0);
 		
-		System.exit(0);
+		return;
 	}
-	
 
 }
